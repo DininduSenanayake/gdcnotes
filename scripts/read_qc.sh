f80adf208a47670a0851f0306ed320d9d1392d6b
Dinindu Senanayake <dinindu.senanayake@gmail.com>
2021-09-22 08:36:02 +1200

    add scripts

---
+++ scripts/read_qc.sh
@@ -0,0 +1,26 @@
+set -e
+
+source ~/modload.sh
+
+cd ~/dc_workshop/data/untrimmed_fastq
+
+echo "Running FastQC"
+fastqc *.fastq*
+
+mkdir -p ~/dc_workshop/results/fastqc_untrimmed_reads
+
+echo "Saving FastQC results.."
+
+mv *.zip ~/dc_workshop/results/fastqc_untrimmed_reads/
+mv *.html ~/dc_workshop/results/fastqc_untrimmed_reads/
+
+cd ~/dc_workshop/results/fastqc_untrimmed_reads/
+
+echo "Unzipping..."
+for filename in *.zip
+do
+    unzip $filename
+done
+
+echo "Saving summary...."
+cat */summary.txt > ~/dc_workshop/docs/fastqc_summaries.txt
